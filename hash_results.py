a6c719102ccb0c962364ccf02ce3786b0ca38dab
brennanleez-coder <brennanlee95@gmail.com>
2023-12-04 15:22:28 +0800

    Update hash_results.py

--- hash_results.py
+++ hash_results.py
@@ -4,13 +4,12 @@ class HashResult(Enum):
     
     PENDING="PENDING: downloaded apk"
     PASSED="Rank 1: PASSED - Same: App Hash, App Cert Hash, Permissions"
-    DIFF_PSACHAH="Rank 2 - DIFF: Permissions, Same: App Cert Hash, App Hash"
-    DIFF_PSACHP="Rank 3 - DIFF: Permissions, Same: App Cert Hash, App Hash"
-    DIFF_ACHAHSP="Rank 4 - DIFF: App Cert Hash, App Hash, Same: Permissions"
-    DIFF_PAHSACH="Rank 5 - DIFF: Permissions, App Hash, Same: App Cert Hash"
-    DIFF_PACHSAH="Rank 6 - DIFF: Permissions, App Cert Hash, Same: App Hash"
-    DIFF_AHPSACH="Rank 7 - DIFF: Permissions, App Hash, Same: App Cert Hash"
-    DIFF_ACHPAH="Rank 8 - DIFF: App Cert Hash, App Hash, Permissions"
+    DIFF_PSACHAH="Rank 2 - DIFF: Permissions |Same: App Cert Hash, App Hash"
+    DIFF_ACHAHSP="Rank 3 - DIFF: App Cert Hash, App Hash | Same: Permissions"
+    DIFF_PAHSACH="Rank 4 - DIFF: Permissions, App Hash | Same: App Cert Hash"
+    DIFF_PACHSAH="Rank 5 - DIFF: Permissions, App Cert Hash | Same: App Hash"
+    DIFF_AHPSACH="Rank 6 - DIFF: Permissions, App Hash | Same: App Cert Hash"
+    DIFF_ACHPAH="Rank 7 - DIFF: App Cert Hash, App Hash, Permissions"
 
 def determine_apk_legitimacy(incoming_apk_hash, downloaded_apk_hash, incoming_app_cert_hash, downloaded_app_cert_hash, incoming_permissions, downloaded_permissions):
     # Using hash_results enum, determine level of apk legitimacy
@@ -23,8 +22,6 @@ def determine_apk_legitimacy(incoming_apk_hash, downloaded_apk_hash, incoming_ap
         return HashResult.PASSED.value
     elif not is_same_permissions and is_same_app_cert_hash and is_same_app_hash:
         return HashResult.DIFF_PSACHAH.value
-    elif not is_same_permissions and is_same_app_cert_hash:
-        return HashResult.DIFF_PSACHP.value
     elif not is_same_app_cert_hash and is_same_app_hash and is_same_permissions:
         return HashResult.DIFF_ACHAHSP.value
     elif not is_same_permissions and is_same_app_hash:
